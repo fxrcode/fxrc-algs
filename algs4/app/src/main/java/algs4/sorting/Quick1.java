b3a460fdb78f2556bdea26b8e1d42fd3e4160718
fxrcode <fxrcode@gmail.com>
2020-11-01 17:02:39 -0800

    600 leetcode based on foundation of algs4

---
+++ algs4/app/src/main/java/algs4/sorting/Quick1.java
@@ -0,0 +1,17 @@
+package algs4.sorting;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Quick1 {
+    public static void main(String[] args) {
+        StdOut.println("Hello Quick");
+//        String[] a = StdIn.readAllStrings();
+        // read data and compute statistics
+        while (!StdIn.isEmpty()) {
+            double value = StdIn.readDouble();
+            StdOut.println(value);
+        }
+        StdOut.println("Done");
+    }
+}
